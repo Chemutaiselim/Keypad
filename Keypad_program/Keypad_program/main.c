bc340e40d3751a73f693fd780fd98acde186c3f2
Selim <selimchemutai19@gmail.com>
2021-12-05 17:46:05 +0300

    Create main.c

---
+++ Keypad_program/Keypad_program/main.c
@@ -0,0 +1,18 @@
+/*
+ * Keypad_program.c
+ *
+ * Created: 05/12/2021 17:34:58
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
